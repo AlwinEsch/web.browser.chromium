9188866fa479329f6924c2a908309f6dbeea7f6f
AlwinEsch <alwin.esch@web.de>
2015-05-30 12:25:16 +0200

    Link to renamed bitbucket account kodi-web

--- versions.cmake
+++ versions.cmake
@@ -12,12 +12,12 @@ set(CEF_COMPLETE_VERSION "2357.1275.gd2d36ca")
 set(CEF_OWN_CHANGES_VERSION "0.0.2")
 if(CEF_COMPLETE)
   set(CEF_BRANCH_VERSION 2357)
-  set(CEF_BRANCH_URL https://bitbucket.org/EsMaSol/cef.git)
+  set(CEF_BRANCH_URL https://bitbucket.org/kodi-web/cef.git)
   add_revision(cef-binary)
 else()
   # CEF_OS_NAME and BITSIZE are defined by CMakeLists.txt
   add_revision(cef-binary
-    URL https://bitbucket.org/EsMaSol/cef/downloads/kodi-web-${CEF_OWN_CHANGES_VERSION}_cef_binary_3_${CEF_COMPLETE_VERSION}_${CEF_OS_NAME}${BITSIZE}.zip
+    URL https://bitbucket.org/kodi-web/cef/downloads/kodi-web-${CEF_OWN_CHANGES_VERSION}_cef_binary_3_${CEF_COMPLETE_VERSION}_${CEF_OS_NAME}${BITSIZE}.zip
     URL_MD5 1570985420a5b6beb285b13f66facd14
   )
 endif()
