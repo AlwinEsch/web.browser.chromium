e57b766bd34f04b4cf79245e05394d604520ede7
AlwinEsch <alwin.esch@web.de>
2015-06-03 05:56:02 +0200

    Add CefMenuModel functions to CWebGUIDialogContextMenu (not coded)

--- src/addon/Dialogs/WebGUIDialogContextMenu.cpp
+++ src/addon/Dialogs/WebGUIDialogContextMenu.cpp
@@ -27,3 +27,335 @@ CWebGUIDialogContextMenu::~CWebGUIDialogContextMenu()
 {
 
 }
+
+bool CWebGUIDialogContextMenu::Clear()
+{
+  return false;
+}
+
+int CWebGUIDialogContextMenu::GetCount()
+{
+  return 0;
+}
+
+bool CWebGUIDialogContextMenu::AddSeparator()
+{
+  return false;
+}
+
+bool CWebGUIDialogContextMenu::AddItem(
+    int              command_id,
+    const CefString& label)
+{
+  return false;
+}
+
+bool CWebGUIDialogContextMenu::AddCheckItem(
+    int              command_id,
+    const CefString& label)
+{
+  return false;
+}
+
+bool CWebGUIDialogContextMenu::AddRadioItem(
+    int              command_id,
+    const CefString& label,
+    int              group_id)
+{
+  return false;
+}
+
+CefRefPtr<CefMenuModel> CWebGUIDialogContextMenu::AddSubMenu(
+    int              command_id,
+    const CefString& label)
+{
+  return nullptr;
+}
+
+bool CWebGUIDialogContextMenu::InsertSeparatorAt(
+    int              index)
+{
+  return false;
+}
+
+bool CWebGUIDialogContextMenu::InsertItemAt(
+    int              index,
+    int              command_id,
+    const CefString& label)
+{
+  return false;
+}
+
+bool CWebGUIDialogContextMenu::InsertCheckItemAt(
+    int              index,
+    int              command_id,
+    const CefString& label)
+{
+  return false;
+}
+
+bool CWebGUIDialogContextMenu::InsertRadioItemAt(
+    int              index,
+    int              command_id,
+    const CefString& label,
+    int              group_id)
+{
+  return false;
+}
+
+CefRefPtr<CefMenuModel> CWebGUIDialogContextMenu::InsertSubMenuAt(
+    int              index,
+    int              command_id,
+    const CefString& label)
+{
+  return nullptr;
+}
+
+bool CWebGUIDialogContextMenu::Remove(
+    int              command_id)
+{
+  return false;
+}
+
+bool CWebGUIDialogContextMenu::RemoveAt(
+    int              index)
+{
+  return false;
+}
+
+int CWebGUIDialogContextMenu::GetIndexOf(
+    int              command_id)
+{
+  return 0;
+}
+
+int CWebGUIDialogContextMenu::GetCommandIdAt(
+    int              index)
+{
+  return 0;
+}
+
+bool CWebGUIDialogContextMenu::SetCommandIdAt(
+    int              index,
+    int              command_id)
+{
+  return false;
+}
+
+CefString CWebGUIDialogContextMenu::GetLabel(
+    int              command_id)
+{
+  return "";
+}
+
+CefString CWebGUIDialogContextMenu::GetLabelAt(
+    int              index)
+{
+  return "";
+}
+
+bool CWebGUIDialogContextMenu::SetLabel(
+    int              command_id,
+    const CefString& label)
+{
+  return false;
+}
+
+bool CWebGUIDialogContextMenu::SetLabelAt(
+    int              index,
+    const CefString& label)
+{
+  return false;
+}
+
+CefMenuModel::MenuItemType CWebGUIDialogContextMenu::GetType(
+    int              command_id)
+{
+  return MENUITEMTYPE_NONE;
+}
+
+CefMenuModel::MenuItemType CWebGUIDialogContextMenu::GetTypeAt(
+    int              index)
+{
+  return MENUITEMTYPE_NONE;
+}
+
+int CWebGUIDialogContextMenu::GetGroupId(
+    int              command_id)
+{
+  return 0;
+}
+
+int CWebGUIDialogContextMenu::GetGroupIdAt(
+    int              index)
+{
+  return 0;
+}
+
+bool CWebGUIDialogContextMenu::SetGroupId(
+    int              command_id,
+    int              group_id)
+{
+  return false;
+}
+
+bool CWebGUIDialogContextMenu::SetGroupIdAt(
+    int              index,
+    int              group_id)
+{
+  return false;
+}
+
+CefRefPtr<CefMenuModel> CWebGUIDialogContextMenu::GetSubMenu(
+    int              command_id)
+{
+  return nullptr;
+}
+
+CefRefPtr<CefMenuModel> CWebGUIDialogContextMenu::GetSubMenuAt(
+    int              index)
+{
+  return nullptr;
+}
+
+bool CWebGUIDialogContextMenu::IsVisible(
+    int              command_id)
+{
+  return false;
+}
+
+bool CWebGUIDialogContextMenu::IsVisibleAt(
+    int              index)
+{
+  return false;
+}
+
+bool CWebGUIDialogContextMenu::SetVisible(
+    int              command_id,
+    bool             visible)
+{
+  return false;
+}
+
+bool CWebGUIDialogContextMenu::SetVisibleAt(
+    int              index,
+    bool             visible)
+{
+  return false;
+}
+
+bool CWebGUIDialogContextMenu::IsEnabled(
+    int              command_id)
+{
+  return false;
+}
+
+bool CWebGUIDialogContextMenu::IsEnabledAt(
+    int              index)
+{
+  return false;
+}
+
+bool CWebGUIDialogContextMenu::SetEnabled(
+    int              command_id,
+    bool             enabled)
+{
+  return false;
+}
+
+bool CWebGUIDialogContextMenu::SetEnabledAt(
+    int              index,
+    bool             enabled)
+{
+  return false;
+}
+
+bool CWebGUIDialogContextMenu::IsChecked(
+    int              command_id)
+{
+  return false;
+}
+
+bool CWebGUIDialogContextMenu::IsCheckedAt(
+    int              index)
+{
+  return false;
+}
+
+bool CWebGUIDialogContextMenu::SetChecked(
+    int              command_id,
+    bool             checked)
+{
+  return false;
+}
+
+bool CWebGUIDialogContextMenu::SetCheckedAt(
+    int              index,
+    bool             checked)
+{
+  return false;
+}
+
+bool CWebGUIDialogContextMenu::HasAccelerator(
+    int              command_id)
+{
+  return false;
+}
+
+bool CWebGUIDialogContextMenu::HasAcceleratorAt(
+    int              index)
+{
+  return false;
+}
+
+bool CWebGUIDialogContextMenu::SetAccelerator(
+    int              command_id,
+    int              key_code,
+    bool             shift_pressed,
+    bool             ctrl_pressed,
+    bool             alt_pressed)
+{
+  return false;
+}
+
+bool CWebGUIDialogContextMenu::SetAcceleratorAt(
+    int              index,
+    int              key_code,
+    bool             shift_pressed,
+    bool             ctrl_pressed,
+    bool             alt_pressed)
+{
+  return false;
+}
+
+bool CWebGUIDialogContextMenu::RemoveAccelerator(
+    int              command_id)
+{
+  return false;
+}
+
+bool CWebGUIDialogContextMenu::RemoveAcceleratorAt(
+    int              index)
+{
+  return false;
+}
+
+bool CWebGUIDialogContextMenu::GetAccelerator(
+    int              command_id,
+    int&             key_code,
+    bool&            shift_pressed,
+    bool&            ctrl_pressed,
+    bool&            alt_pressed)
+{
+  return false;
+}
+
+bool CWebGUIDialogContextMenu::GetAcceleratorAt(
+    int              index,
+    int&             key_code,
+    bool&            shift_pressed,
+    bool&            ctrl_pressed,
+    bool&            alt_pressed)
+{
+  return false;
+}
--- src/addon/Dialogs/WebGUIDialogContextMenu.h
+++ src/addon/Dialogs/WebGUIDialogContextMenu.h
@@ -17,10 +17,197 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-class CWebGUIDialogContextMenu
+#include "include/cef_menu_model.h"
+
+class CWebGUIDialogContextMenu : CefMenuModel
 {
 public:
   CWebGUIDialogContextMenu();
   virtual ~CWebGUIDialogContextMenu();
 
+  virtual bool Clear()                                                ///!< Clears the menu. Returns true on success.
+                        OVERRIDE;
+  virtual int GetCount()                                              ///!< Returns the number of items in this menu.
+                        OVERRIDE;
+  virtual bool AddSeparator()                                         ///!< Add a separator to the menu. Returns true on success.
+                        OVERRIDE;
+  virtual bool AddItem(                                               ///!< Add an item to the menu. Returns true on success.
+      int                                   command_id,               ///
+      const CefString&                      label)                    ///
+                        OVERRIDE;
+  virtual bool AddCheckItem(                                          ///!< Add a check item to the menu. Returns true on success.
+      int                                   command_id,               ///
+      const CefString&                      label)                    ///
+                        OVERRIDE;
+  virtual bool AddRadioItem(                                          ///!< Add a radio item to the menu. Only a single item with the specified
+      int                                   command_id,               ///   |group_id| can be checked at a time. Returns true on success.
+      const CefString&                      label,                    ///
+      int                                   group_id)                 ///
+                        OVERRIDE;
+  virtual CefRefPtr<CefMenuModel> AddSubMenu(                         ///!< Add a sub-menu to the menu. The new sub-menu is returned.
+      int                                   command_id,               ///
+      const CefString&                      label)                    ///
+                        OVERRIDE;
+  virtual bool InsertSeparatorAt(                                     ///!< Insert a separator in the menu at the specified |index|. Returns true on success.
+      int                                   index)                    ///
+                        OVERRIDE;
+  virtual bool InsertItemAt(                                          ///!< Insert an item in the menu at the specified |index|. Returns true on success
+      int                                   index,                    ///
+      int                                   command_id,               ///
+      const CefString&                      label)                    ///
+                        OVERRIDE;
+  virtual bool InsertCheckItemAt(                                     ///!< Insert a check item in the menu at the specified |index|. Returns true on success.
+      int                                   index,                    ///
+      int                                   command_id,               ///
+      const CefString&                      label)                    ///
+                        OVERRIDE;
+  virtual bool InsertRadioItemAt(                                     ///!< Insert a radio item in the menu at the specified |index|. Only a single item
+      int                                   index,                    ///   with the specified |group_id| can be checked at a time. Returns true on success.
+      int                                   command_id,               ///
+      const CefString&                      label,                    ///
+      int                                   group_id)                 ///
+                        OVERRIDE;
+  virtual CefRefPtr<CefMenuModel> InsertSubMenuAt(                    ///!< Insert a sub-menu in the menu at the specified |index|. The new sub-menu
+      int                                   index,                    ///   is returned.
+      int                                   command_id,               ///
+      const CefString&                      label)                    ///
+                        OVERRIDE;
+  virtual bool Remove(                                                ///!< Removes the item with the specified |command_id|. Returns true on success.
+      int                                   command_id)               ///
+                        OVERRIDE;
+  virtual bool RemoveAt(                                              ///!< Removes the item at the specified |index|. Returns true on success.
+      int                                   index)                    ///
+                        OVERRIDE;
+  virtual int GetIndexOf(                                             ///!< Returns the index associated with the specified |command_id| or -1 if not found
+      int                                   command_id)               ///   due to the command id not existing in the menu.
+                        OVERRIDE;
+  virtual int GetCommandIdAt(                                         ///!< Returns the command id at the specified |index| or -1 if not found due to
+      int                                   index)                    ///   invalid range or the index being a separator.
+                        OVERRIDE;
+  virtual bool SetCommandIdAt(                                        ///!< Sets the command id at the specified |index|. Returns true on success.
+      int                                   index,                    ///
+      int                                   command_id)               ///
+                        OVERRIDE;
+  virtual CefString GetLabel(                                         ///!< Returns the label for the specified |command_id| or empty if not found.
+      int                                   command_id)               ///
+                        OVERRIDE;
+  virtual CefString GetLabelAt(                                       ///!< Returns the label at the specified |index| or empty if not found due to
+      int                                   index)                    ///   invalid range or the index being a separator.
+                        OVERRIDE;
+  virtual bool SetLabel(                                              ///!< Sets the label for the specified |command_id|. Returns true on success.
+      int                                   command_id,               ///
+      const CefString&                      label)                    ///
+                        OVERRIDE;
+  virtual bool SetLabelAt(                                            ///!< Set the label at the specified |index|. Returns true on success.
+      int                                   index,                    ///
+      const CefString&                      label)                    ///
+                        OVERRIDE;
+  virtual MenuItemType GetType(                                       ///!< Returns the item type for the specified |command_id|. default_retval=MENUITEMTYPE_NONE
+      int                                   command_id)               ///
+                        OVERRIDE;
+  virtual MenuItemType GetTypeAt(                                     ///!< Returns the item type for the specified |index|. default_retval=MENUITEMTYPE_NONE
+      int                                   index)                    ///
+                        OVERRIDE;
+  virtual int GetGroupId(                                             ///!< Returns the group id for the specified |command_id| or -1 if invalid.
+      int                                   command_id)               ///
+                        OVERRIDE;
+  virtual int GetGroupIdAt(                                           ///!< Returns the group id at the specified |index| or -1 if invalid.
+      int                                   index)                    ///
+                        OVERRIDE;
+  virtual bool SetGroupId(                                            ///!< Sets the group id for the specified |command_id|. Returns true on success.
+      int                                   command_id,               ///
+      int                                   group_id)                 ///
+                        OVERRIDE;
+  virtual bool SetGroupIdAt(                                          ///!< Sets the group id at the specified |index|. Returns true on success.
+      int                                   index,                    ///
+      int                                   group_id)                 ///
+                        OVERRIDE;
+  virtual CefRefPtr<CefMenuModel> GetSubMenu(                         ///!< Returns the submenu for the specified |command_id| or empty if invalid.
+      int                                   command_id)               ///
+                        OVERRIDE;
+  virtual CefRefPtr<CefMenuModel> GetSubMenuAt(                       ///!< Returns the submenu at the specified |index| or empty if invalid.
+      int                                   index)                    ///
+                        OVERRIDE;
+  virtual bool IsVisible(                                             ///!< Returns true if the specified |command_id| is visible.
+      int                                   command_id)               ///
+                        OVERRIDE;
+  virtual bool IsVisibleAt(                                           ///!< Returns true if the specified |index| is visible.
+      int                                   index)                    ///
+                        OVERRIDE;
+  virtual bool SetVisible(                                            ///!< Change the visibility of the specified |command_id|. Returns true on success.
+      int                                   command_id,               ///
+      bool                                  visible)                  ///
+                        OVERRIDE;
+  virtual bool SetVisibleAt(                                          ///!< Change the visibility at the specified |index|. Returns true on success.
+      int                                   index,                    ///
+      bool                                  visible)                  ///
+                        OVERRIDE;
+  virtual bool IsEnabled(                                             ///!< Returns true if the specified |command_id| is enabled.
+      int                                   command_id)               ///
+                        OVERRIDE;
+  virtual bool IsEnabledAt(                                           ///!< Returns true if the specified |index| is enabled.
+      int                                   index)                    ///
+                        OVERRIDE;
+  virtual bool SetEnabled(                                            ///!< Change the enabled status of the specified |command_id|. Returns true on success.
+      int                                   command_id,               ///
+      bool                                  enabled)                  ///
+                        OVERRIDE;
+  virtual bool SetEnabledAt(                                          ///!< Change the enabled status at the specified |index|. Returns true on success.
+      int                                   index,                    ///
+      bool                                  enabled)                  ///
+                        OVERRIDE;
+  virtual bool IsChecked(                                             ///!< Returns true if the specified |command_id| is checked. Only applies to
+      int                                   command_id)               ///   check and radio items.
+                        OVERRIDE;
+  virtual bool IsCheckedAt(                                           ///!< Returns true if the specified |index| is checked. Only applies to check
+      int                                   index)                    ///   and radio items.
+                        OVERRIDE;
+  virtual bool SetChecked(                                            ///!< Check the specified |command_id|. Only applies to check and radio items.
+      int                                   command_id,               ///   Returns true on success.
+      bool                                  checked)                  ///
+                        OVERRIDE;
+  virtual bool SetCheckedAt(                                          ///!< Check the specified |index|. Only applies to check and radio items.
+      int                                   index,                    ///   Returns true on success.
+      bool                                  checked)                  ///
+                        OVERRIDE;
+  virtual bool HasAccelerator(                                        ///!< Returns true if the specified |command_id| has a keyboard accelerator assigned.
+      int                                   command_id)               ///
+                        OVERRIDE;
+  virtual bool HasAcceleratorAt(                                      ///!< Returns true if the specified |index| has a keyboard accelerator assigned.
+      int                                   index)                    ///
+                        OVERRIDE;
+  virtual bool SetAccelerator(                                        ///!< Set the keyboard accelerator for the specified |command_id|. |key_code| can
+      int                                   command_id,               ///   be any virtual key or character value. Returns true on success.
+      int                                   key_code,                 ///
+      bool                                  shift_pressed,            ///
+      bool                                  ctrl_pressed,             ///
+      bool                                  alt_pressed)              ///
+                        OVERRIDE;
+  virtual bool SetAcceleratorAt(                                      ///!< Set the keyboard accelerator at the specified |index|. |key_code| can be
+      int                                   index,                    ///   any virtual key or character value. Returns true on success.
+      int                                   key_code,                 ///
+      bool                                  shift_pressed,            ///
+      bool                                  ctrl_pressed,             ///
+      bool                                  alt_pressed)              ///
+                        OVERRIDE;
+  virtual bool RemoveAccelerator(                                     ///!< Remove the keyboard accelerator for the specified |command_id|. Returns
+      int                                   command_id)               ///   true on success.
+                        OVERRIDE;
+  virtual bool RemoveAcceleratorAt(                                   ///!< Remove the keyboard accelerator at the specified |index|. Returns true on success.
+      int                                   index)                    ///
+                        OVERRIDE;
+  virtual bool GetAccelerator(                                        ///!< Retrieves the keyboard accelerator for the specified |command_id|. Returns
+      int                                   command_id,               ///   true on success.
+      int&                                  key_code,                 ///
+      bool&                                 shift_pressed,            ///
+      bool&                                 ctrl_pressed,             ///
+      bool&                                 alt_pressed)              ///
+                        OVERRIDE;
+  virtual bool GetAcceleratorAt(                                      ///!< Retrieves the keyboard accelerator for the specified |index|.
+      int                                   index,                    ///   Returns true on success.
+      int&                                  key_code,                 ///
+      bool&                                 shift_pressed,            ///
+      bool&                                 ctrl_pressed,             ///
+      bool&                                 alt_pressed)              ///
+                        OVERRIDE;
 };
