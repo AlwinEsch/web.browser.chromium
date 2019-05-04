6645fae9f6e3bc8016c7fbee5200374f702539b9
Alwin Esch <alwin.esch@web.de>
2019-05-04 02:39:02 +0200

    remove set of sandbox to root
    Currently it seems to not needed to set root rights to them.
    But this is not complete confirmed, maybe this comes back.
    
    https://chromium.googlesource.com/chromium/src/+/master/docs/linux_suid_sandbox_development.md

--- src/addon/addon.cpp
+++ src/addon/addon.cpp
@@ -11,7 +11,6 @@
 #include "AppBrowser.h"
 #include "MessageIds.h"
 #include "RequestContextHandler.h"
-#include "SandboxControl.h"
 #include "WebBrowserClient.h"
 #include "WidevineControl.h"
 #include "utils/Utils.h"
@@ -52,10 +51,6 @@ WEB_ADDON_ERROR CWebBrowser::StartInstance()
     return WEB_ADDON_ERROR_FAILED;
   }
 #endif
-
-  // Check set of sandbox and if needed ask user about root password to set correct rights of them
-//   if (!SandboxControl::SetSandbox())
-//     return WEB_ADDON_ERROR_FAILED;
 
   // Set download path if not available
   if (kodi::GetSettingString("downloads.path").empty())
