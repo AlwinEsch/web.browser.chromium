871d10010f39ef65f5e4190085033fb352d5cb01
Alwin Esch <alwin.esch@web.de>
2019-05-21 01:56:20 +0200

    fix Mac part

--- depends/common/chromium/CMake/copy-cef.cmake
+++ depends/common/chromium/CMake/copy-cef.cmake
@@ -1,6 +1,10 @@
 file(STRINGS ${PROJECT_BINARY_DIR}/cef_version.txt CEF_VERSION)
 
-set(PACKAGE_PLACE "${PROJECT_BINARY_DIR}/chromium_package/cef_binary_${CEF_VERSION}_${CORE_SYSTEM_NAME}${CEF_BIT_STR}")
+if(CORE_SYSTEM_NAME STREQUAL osx)
+  set(PACKAGE_PLACE "${PROJECT_BINARY_DIR}/chromium_package/cef_binary_${CEF_VERSION}_macosx${CEF_BIT_STR}")
+else()
+  set(PACKAGE_PLACE "${PROJECT_BINARY_DIR}/chromium_package/cef_binary_${CEF_VERSION}_${CORE_SYSTEM_NAME}${CEF_BIT_STR}")
+endif()
 
 message(STATUS "Used package: ${PACKAGE_PLACE}")
 message(STATUS "   copied to: ${SOURCE_DIR}")
