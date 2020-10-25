6d53c00e789ab4bea066c8ecf111803aeeec3d16
Alwin Esch <alwin.esch@web.de>
2020-10-25 15:31:45 +0100

    fix demo python addon

--- test/script.hello.web.world/addon.py
+++ test/script.hello.web.world/addon.py
@@ -2,6 +2,6 @@ import xbmcaddon
 import xbmcgui
 
 if __name__ == '__main__':
-    dialog = xbmcgui.WindowXMLDialog('browser.xml', xbmcaddon.Addon().getAddonInfo('path').decode('utf-8'), 'default', '1080p')
+    dialog = xbmcgui.WindowXMLDialog('browser.xml', xbmcaddon.Addon().getAddonInfo('path'), 'default', '1080p')
     dialog.doModal()
     del dialog
